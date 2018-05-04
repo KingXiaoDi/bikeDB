dad5c4e1e3580b5ddbc16217216653913447f423
Yoshi <KingXiaoDi@users.noreply.github.com>
2018-05-04 15:34:28 -0400

    updated bike CSV so distance has more digits

--- temp.py
+++ temp.py
@@ -6,9 +6,10 @@ file = 'c:/users/josh/documents/python/biking/biking.csv'
 bikeDF = pandas.read_csv(file, index_col=0)
 bikeDF['Date'] = pandas.to_datetime(bikeDF['Date'])
 #bikeDF = bikeDF.replace(['Lnth LR'], 'Linthicum Light Rail')
-#bikeDF.to_csv(file)
+bikeDF.to_csv(file)
 
 for year in range(2017, 2018):
+	pass
 	df = bikeDF.loc[bikeDF['Date'].dt.year == year].reset_index(drop=True)
-	print ()
-	print ()
+	#print ()
+	#print ()
