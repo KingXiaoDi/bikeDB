b4991408380caa5b242c07df2be18f4a4b5bd1f7
Yoshi <organizedchaos20@gmail.com>
2018-05-10 22:00:30 -0400

    adjusted code to allow me to upload CSV to SQL; SQL is up-to-date on rides as of 2018-05-10

--- CREATE TABLE Rides.sql
+++ CREATE TABLE Rides.sql
@@ -1,8 +1,9 @@
+USE biking;
 CREATE TABLE Rides (
     RideID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
     RideDate date NOT NULL,
-    StartPoint nvarchar(30) NOT NULL,
-    EndPoint nvarchar(30) NOT NULL,
+    LocationStart nvarchar(30) NOT NULL,
+    LocationEnd nvarchar(30) NOT NULL,
     Distance decimal(8,4) NOT NULL,
-    StartTime time,
-    EndTime time);
+    TimeStart time,
+    TimeEnd time);
--- temp.py
+++ temp.py
@@ -1,15 +1,33 @@
+from mySQL import createConnection, closeConnection, query
 import datetime
 import pandas
+import numpy
 
-file = 'c:/users/josh/documents/python/biking/biking.csv'
+def loadCSV(name):
+	bikeDF = pandas.read_csv(file, index_col=0).sort_values(by='RideDate')
+	bikeDF['RideDate'] = pandas.to_datetime(bikeDF['RideDate'])
+	return bikeDF
 
-bikeDF = pandas.read_csv(file, index_col=0)
-bikeDF['Date'] = pandas.to_datetime(bikeDF['Date'])
-#bikeDF = bikeDF.replace(['Lnth LR'], 'Linthicum Light Rail')
-bikeDF.to_csv(file)
+def uploadDFtoSQL(df, con):
+	print ('Attempting to upload df to MySQL')
+	try: 
+		df.to_sql('rides', con, if_exists='append', index=False)
+		print ('Upload successful.')
+	except Error as e:
+		print ('Upload failed.')
+		print (e)
 
-for year in range(2017, 2018):
-	pass
-	df = bikeDF.loc[bikeDF['Date'].dt.year == year].reset_index(drop=True)
-	#print ()
-	#print ()
+def prepareForUpload(df, con):
+	for year in range(2016, 2019):
+		df = bikeDF.loc[bikeDF['RideDate'].dt.year == year].reset_index(drop=True)
+		uploadDFtoSQL(df, con)
+
+def saveCSV(df, name):
+	df.to_csv(file)
+		
+#file = 'c:/users/josh/documents/python/biking/biking.csv'
+file = 'c:/users/yoshi/documents/python_scripts/biking/biking.csv'
+	
+connection = createConnection('bikeUpload', 'biking')
+query(connection, 'SELECT * FROM rides')
+closeConnection(connection)
